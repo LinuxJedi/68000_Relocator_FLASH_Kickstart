9a5c41ecf71dcca1f524c211396701693563f970
Andrew Hutchings <andrew@linuxjedi.co.uk>
2021-02-13 23:49:14 +0000

    Add missing license header

--- Software/gui.c
+++ Software/gui.c
@@ -1,3 +1,22 @@
+/*
+    This file the FLASH_KICKSTART GUI.
+
+    This file was created by Andrew (LinuxJedi) Hutchings.
+
+    FLASH_KICKSTART is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    FLASH_KICKSTART is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLASH_KICKSTART. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <proto/intuition.h>
 #include <proto/dos.h>
 #include <intuition/intuition.h>
